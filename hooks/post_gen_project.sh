c1dfed9f2174ce82f06db03f4d1a177a8936ea4c
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2023-02-27 14:18:09 -0500

    build(COOKIECUTTER): add update script

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,45 +1,71 @@
 #!/bin/bash
 
+# hooks/post_gen_project.sh
+# Configures the templated profile for use.
+
+# PROFILE_GENERATOR_BRANCH_NAME_BASE:         Optional alternate base branch name.
+# PROFILE_GENERATOR_BRANCH_NAME_DEVELOPMENT:  Optional alternate development branch name.
+# PROFILE_GENERATOR_SKIP_GIT_INIT:            Optionally set to 1 to skip creating branches and initial commit.
+# PROFILE_GENERATOR_SKIP_POETRY:              Optionally set to 1 to skip installing dependencies.
+# PROFILE_GENERATOR_SKIP_PRECOMMIT:           Optionally set to 1 to skip installing pre-commit hooks.
+
+# cookiecutter only script.
+
 set -eo pipefail
 
 PROFILE_GENERATOR_BRANCH_NAME_BASE=${PROFILE_GENERATOR_BRANCH_NAME_BASE-"{{ cookiecutter._BRANCH_NAME_BASE }}"}
 PROFILE_GENERATOR_BRANCH_NAME_DEVELOPMENT=${PROFILE_GENERATOR_BRANCH_NAME_DEVELOPMENT-"{{ cookiecutter._BRANCH_NAME_DEVELOPMENT }}"}
+PROFILE_GENERATOR_TEMPLATE_URL="https://github.com/osx-provisioner/profile-generator.git"
 
 initialize_git() {
-  git init
-  git checkout -b "${PROFILE_GENERATOR_BRANCH_NAME_BASE}"
-  git stage .
-  git commit -m "build(COOKIECUTTER): initial generation"
-  git symbolic-ref HEAD "refs/heads/${PROFILE_GENERATOR_BRANCH_NAME_BASE}"
-  git tag 0.0.0
-  git checkout -b "${PROFILE_GENERATOR_BRANCH_NAME_DEVELOPMENT}"
-  mkdir -p files templates
+
+  if [[ "${PROFILE_GENERATOR_SKIP_GIT_INIT}" != "1" ]]; then
+    git init
+    git checkout -b "${PROFILE_GENERATOR_BRANCH_NAME_BASE}"
+    git stage .
+    git commit -m "build(COOKIECUTTER): initial generation"
+    git symbolic-ref HEAD "refs/heads/${PROFILE_GENERATOR_BRANCH_NAME_BASE}"
+    git tag 0.0.0
+    git checkout -b "${PROFILE_GENERATOR_BRANCH_NAME_DEVELOPMENT}"
+    mkdir -p files templates
+  fi
+
 }
 
 initialize_poetry() {
+
   if [[ "${PROFILE_GENERATOR_SKIP_POETRY}" != "1" ]]; then
     poetry install
   fi
+
 }
 
 initialize_precommit() {
+
   if [[ "${PROFILE_GENERATOR_SKIP_PRECOMMIT}" != "1" ]]; then
     poetry run pre-commit install -t pre-commit -t commit-msg
     poetry run ansible-galaxy install --timeout 90 -r profile/requirements.yml
   fi
+
 }
 
 update_template_values() {
-  # Compatible with Linux and BSD sed
-  sed -i.bak 's/profile-generator\//https:\/\/github.com\/osx-provisioner\/profile-generator.git/' .cookiecutter/cookiecutter.json
-  rm .cookiecutter/cookiecutter.json.bak
+
+  if ! grep "${PROFILE_GENERATOR_TEMPLATE_URL}" .cookiecutter/cookiecutter.json; then
+    # sed compatible with Linux and BSD
+    sed -i.bak 's,"_template": ".*","_template": "'"${PROFILE_GENERATOR_TEMPLATE_URL}"'",g' .cookiecutter/cookiecutter.json
+    rm .cookiecutter/cookiecutter.json.bak
+  fi
+
 }
 
 main() {
+
   update_template_values
   initialize_git
   initialize_poetry
   initialize_precommit
+
 }
 
 main
---
+++ scripts/update.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# This script helps automate the process of updating a profile that has already been created.
+# A branch "update-template" is created, with the changes required to update the profile.
+# Use git cherry-pick (or create a patch from this change set) to update your Mac Maker profile.
+
+# Before getting started carefully look at your profile's .cookiecutter/cookiecutter.json file.
+# Compare it to the newest version of the template, and update any GitHub workflow action versions and add any
+# new values that have since been included.
+
+# Requires: https://pypi.org/project/cookiecutter-project-upgrader/
+
+# 1:                                  The local filepath to the profile you'll be upgrading.
+# 2:                                  The tag or branch of this repository you'll be using to perform the upgrade.
+# PROFILE_GENERATOR_TEMPLATE_SOURCE:  Optionally repoint to forked remote repository, or a local zip bundle for the template.
+# PROFILE_GENERATOR_UPDATE_BRANCH:    Optionally set a a branch name that will be created to house the proposed changes.
+
+# Experimental end-user script.
+
+set -eo pipefail
+
+PROFILE_GENERATOR_TEMPLATE_SOURCE="${PROFILE_GENERATOR_TEMPLATE_SOURCE-"https://github.com/osx-provisioner/profile-generator.git"}"
+PROFILE_GENERATOR_UPDATE_BRANCH="${PROFILE_GENERATOR_UPDATE_BRANCH-"update-template"}"
+
+
+error() {
+  echo "USAGE: ./update.sh [ROLE FOLDER] [TEMPLATE TAG or BRANCH] [--force]"
+  exit 127
+}
+
+[[ -z $2 ]] && error
+[[ -z $1 ]] && error
+
+main() {
+
+  pushd "$1" || error
+    if [[ "$3" == "--force" ]]; then
+      if git branch | grep "${PROFILE_GENERATOR_UPDATE_BRANCH}"; then
+        set +eo pipefail
+          git checkout master
+          git reset origin/master
+          git clean -fd .
+          git branch -D "${PROFILE_GENERATOR_UPDATE_BRANCH}"
+          rm -rf .git/cookiecutter
+        set -eo pipefail
+      fi
+    fi
+
+    PROFILE_GENERATOR_SKIP_GIT_INIT=1                                     \
+    PROFILE_GENERATOR_SKIP_POETRY=1                                       \
+    PROFILE_GENERATOR_SKIP_PRECOMMIT=1                                    \
+    cookiecutter_project_upgrader                                         \
+      -c .cookiecutter/cookiecutter.json                                  \
+      -b "${PROFILE_GENERATOR_UPDATE_BRANCH}"                             \
+      -u "$2"                                                             \
+      -f "${PROFILE_GENERATOR_TEMPLATE_SOURCE}"                           \
+      -e "profile"                                                        \
+      -e ".gitignore"                                                     \
+      -e "LICENSE"                                                        \
+      -e "README.md"
+
+     git checkout update-template
+
+    echo -e "\n==========="
+    echo -e "\nThe following files differ from the template's newest version:"
+    git diff HEAD~1 --summary
+    echo -e "\nPlease review these changes carefully, and exit from this shell when finished.  Nothing has been pushed or merged yet."
+
+    bash
+
+  popd || true
+
+}
+
+main "$@"
