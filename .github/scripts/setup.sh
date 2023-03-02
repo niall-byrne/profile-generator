2530f373cdf80b42ca6e0721d463be848327b649
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2023-03-02 18:27:32 -0500

    ci(GITHUB): centralize end-user configuration

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -9,8 +9,11 @@ set -eo pipefail
 
 main() {
 
-  PROJECT_NAME="{{ cookiecutter.profile_slug }}"
-  USER_NAME="{{ cookiecutter.github_handle }}"
+  PROJECT_NAME="profile-generator"
+  PROFILE_GENERATOR_BRANCH_NAME_BASE="master"
+  PROFILE_GENERATOR_BRANCH_NAME_DEVELOPMENT="dev"
+  TEMPLATED_NAME="profile-example"
+  USER_NAME="osx-provisioner"
 
   BRANCH_OR_TAG="$(echo "${GITHUB_REF}" | sed 's/refs\/heads\///g' | sed 's/refs\/tags\///g')"
   WORKFLOW_URL="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
@@ -19,7 +22,10 @@ main() {
     echo "BRANCH_OR_TAG=${BRANCH_OR_TAG}"
     echo "CACHE_TTL=$(date +%d)"
     echo "NOTIFICATION=${PROJECT_NAME} [<${WORKFLOW_URL}|${BRANCH_OR_TAG}>]"
+    echo "PROFILE_GENERATOR_BRANCH_NAME_BASE=${PROFILE_GENERATOR_BRANCH_NAME_BASE}"
+    echo "PROFILE_GENERATOR_BRANCH_NAME_DEVELOPMENT=${PROFILE_GENERATOR_BRANCH_NAME_DEVELOPMENT}"
     echo "PROJECT_NAME=${PROJECT_NAME}"
+    echo "TEMPLATED_NAME=${TEMPLATED_NAME}"
     echo "USER_NAME=${USER_NAME}"
     echo "WEBHOOK_URL=${WEBHOOK_URL}"
   } >> "$GITHUB_ENV"
