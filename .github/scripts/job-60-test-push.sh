37c87be0e00c40aa9af671e6a8fac25d96c33d35
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2023-03-10 15:44:10 -0500

    ci(GITHUB): refactor create release

--- .github/scripts/job-60-test-push.sh
+++ .github/scripts/job-60-test-push.sh
@@ -1,26 +1,59 @@
 #!/bin/bash
 
-# .github/scripts/job-60-prune-test-releases.sh
-# Remove existing releases on the test repository.
+# .github/scripts/job-60-test-push.sh
+# Performs pushes against the test repository to trigger rendered workflows.
 
-# GITHUB_TOKEN:   The token used to authorize the call.
-# REMOTE_ORIGIN:  The git remote repository name (organization/repo).
-# TEST_PUSH_TAG:  The tag name to clean up.
+# 1:              The name of the branch you wish to push.
+# 2:              Optionally set a tag you'd like to push.
+# REMOTE_TOKEN:   The auth token that will be used to push.
+# REMOTE_ORIGIN:  The remote repository we're pushing to. (format: "owner/repository")
+# TEMPLATED_NAME: The name of the rendered test project.
 
 # CI only script.
 
 set -eo pipefail
 
-check_releases() {
-  gh release view -R "${REMOTE_ORIGIN}" "${TEST_PUSH_TAG}"
+add_test_commit() {
+  echo "test commit" > test_file.txt
+  git add test_file.txt
+  git commit -m 'feat(TEST_FILE): add test file'
+}
+
+push() {
+  # 1:  The branch you are pushing.
+  git push "$(util_get_remote)" HEAD:"${1}" --force
+}
+
+push_tags() {
+  # 1:  The tag you'd like to push
+
+  # Don't push this tag again.
+  git tag --delete 0.0.0
+
+  # Ensure remote tag doesn't exist
+  set +e
+    git push --delete "$(util_get_remote)" "${1}"
+  set -e
+
+  git tag "${1}"
+  git push "$(util_get_remote)" --tags
+}
+
+util_get_remote() {
+  echo "https://${REMOTE_TOKEN}@github.com/${REMOTE_ORIGIN}.git"
 }
 
 main() {
 
-  while check_releases; do
-    sleep 0.5
-    gh release delete -R "${REMOTE_ORIGIN}" -y "${TEST_PUSH_TAG}"
-  done
+  pushd "${TEMPLATED_NAME}"
+    git checkout "${1}"
+    if [[ -z "${2}" ]]; then
+      add_test_commit
+      push "${1}"
+    else
+      push_tags "${2}"
+    fi
+  popd
 
 }
 
